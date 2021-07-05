0cacdea9be681f21bb47a0455c0db60f849de07b
Tosh <tosh.robertsbrockway@gmail.com>
2021-07-05 14:40:57 -0700

    Implemented notes, working very basically

--- src/App.js
+++ src/App.js
@@ -4,13 +4,37 @@ import SimpleHeader from "./SimpleHeader";
 import NewNote from "./NewNote";
 
 const App = () => {
+
+  //State used to store notes
+  const [notes, setNotes] = useState([]);
+
+  //Return statement: includes SimpleHeader and Notes
   return (
     <div>
-      <SimpleHeader title="Simple To-Do List" subtitle="Written by Tosh Roberts Brockway" lists={["first","second","adsas","sadasd","ggg","hhh",]}/>
-      <NewNote />
+      <SimpleHeader title="Simple To-Do List" 
+                    subtitle="Written by Tosh Roberts Brockway" 
+                    lists={["first","second","adsas","sadasd","ggg","hhh",]}/>
+      
+      {
+        //Render NewNote && pass the notes state and setter function to NewNote
+      }
+      <NewNote notes={notes} setNotes={setNotes} />
+    
+      {
+        //Map over the notes and render each of them
+      }
+      {
+        notes.map((note, i) => (
+          <div key={i}>
+            <div>{note.noteTitle}</div>
+            <div>{note.noteDate}</div>
+            <div>{note.noteContent}</div>
+          </div>  
+        ))
+      }
     </div>
   )
 
 }
   
-  render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
--- src/NewNote.js
+++ src/NewNote.js
@@ -4,8 +4,29 @@ import React, { useState } from 'react';
 // built 7/1/21
 
 const NewNote = (props) => {
+    //Take the form values and set them using the setNotes state setter
+    const onSubmit = (e) => {
+        //Use preventDefault to stop event from replacing the notes that are already there?
+        e.preventDefault();
+
+        //Get and store the values of the form by name
+        const noteTitle = e.target.noteTitle.value;
+        const noteDate = e.target.noteDate.value;
+        const noteContent = e.target.noteContent.value;
+
+        //Take the values and turn them into another variable 
+        //that contains the existing notes & the new note submitted 
+        const newNotes = [...props.notes, {noteTitle, noteDate, noteContent}];
+
+        //use the notes setter to update the notes
+        props.setNotes(newNotes);
+    };
+    
+    
+    
+    //Render NewNote form
     return (
-        <div className="note" id={props.noteId}>
+        <div className="note" onSubmit={onSubmit}>
             <form> 
                 <label htmlFor="noteTitle">Note Title</label>
                 <input type="text" id="noteTitle" name="noteTitle" required />
@@ -15,9 +36,11 @@ const NewNote = (props) => {
 
                 <label htmlFor="noteContent">Note Content</label>
                 <input type="text" id="noteContent" name="noteContent" required />
+
+                <button type="submit">Submit Note</button>
             </form>
         </div>
-    )
-}
+    );
+};
 
 export default NewNote;
